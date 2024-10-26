e56712b3d39cf9b0f4528c1a2a9bc352467e21fd
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-10-26 22:16:28 +0200

    added question sign to make all Models name nullable

--- ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/Models/Category.cs
@@ -9,12 +9,12 @@ namespace ExpenseTracker.Models
         public int CategoryId { get; set; }
 
         [Column(TypeName ="nvarchar(50)")]
-        public string Title { get; set; }
+        public string ?Title { get; set; }
 
         [Column(TypeName ="nvarchar(5)")]
-        public string Icon { get; set; }
+        public string ?Icon { get; set; }
 
         [Column(TypeName = "nvarchar(10)")]
-        public string Type { get; set; }
+        public string ?Type { get; set; }
     }
 }
