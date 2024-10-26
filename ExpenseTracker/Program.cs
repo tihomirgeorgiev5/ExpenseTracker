2d9e844516697297544fbe58cd42cb12c6211a2a
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-10-26 23:33:32 +0200

    Added DbContext

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,9 +1,14 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
+// Dependency Injection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
