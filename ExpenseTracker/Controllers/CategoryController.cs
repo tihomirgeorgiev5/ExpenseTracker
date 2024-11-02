08ac240e4bbddb4a62c5db573be96701b8674fcb
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-11-02 01:20:00 +0100

    optimize ControlView model

--- ExpenseTracker/Controllers/CategoryController.cs
+++ ExpenseTracker/Controllers/CategoryController.cs
@@ -45,7 +45,7 @@ namespace ExpenseTracker.Controllers
         // GET: Category/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new Category());
         }
 
         // POST: Category/Create
