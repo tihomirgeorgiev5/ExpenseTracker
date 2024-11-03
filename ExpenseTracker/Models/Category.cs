213642001e118de809c5a4d8ca4f791c3294629e
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-11-03 11:00:52 +0100

    Added new visibility of Income Expense buttons

--- ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/Models/Category.cs
@@ -11,10 +11,10 @@ namespace ExpenseTracker.Models
         [Column(TypeName ="nvarchar(50)")]
         public string? Title { get; set; }
 
-        [Column(TypeName ="nvarchar(5)")]
-        public string? Icon { get; set; }
+        [Column(TypeName = "nvarchar(5)")]
+        public string? Icon { get; set; } = "";
 
         [Column(TypeName = "nvarchar(10)")]
-        public string? Type { get; set; }
+        public string? Type { get; set; } = "Expense";
     }
 }
