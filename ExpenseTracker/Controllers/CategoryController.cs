444baab71c900dc50a7b27e9261ea2182312a911
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-11-03 22:16:08 +0100

    added model for CategoryId to can added all variants of input

--- ExpenseTracker/Controllers/CategoryController.cs
+++ ExpenseTracker/Controllers/CategoryController.cs
@@ -65,7 +65,15 @@ namespace ExpenseTracker.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(category);
+                if (category.CategoryId==0)
+                {
+                    _context.Add(category);
+                }
+                else
+                {
+                    _context.Update(category);
+                }
+                
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
